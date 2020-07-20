15c4e0979c9848a2f2dfb2bd89759f2944fa8bee
Aleksandr Hrytsenko <oleksandrhry@wizardsdev.com>
2020-07-20 18:24:57 +0300

    add simple routing

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { MyForm, MyFormWithWatch, FormWithTrigger } from './components/ReactFormHook/ReactFormHook.jsx';
+import { FormikForm } from './components/Formik/FormikForm.jsx';
+
+export const Form() {
+  return (
+    <div className="App">
+        <div className="react-hook-form">
+            <h3>React-hook-form</h3>
+            <div className="form">
+                <MyForm/>
+            </div>
+            <div className="form">
+                <MyFormWithWatch/>
+            </div>
+            <div className="form">
+                <FormWithTrigger/>
+            </div>
+        </div>
+        <div className="formik">
+            <h3>Formik</h3>
+            <FormikForm/>
+        </div>
+    </div>
+  );
+}
