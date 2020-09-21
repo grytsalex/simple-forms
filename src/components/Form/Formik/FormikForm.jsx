7f807aec59f4bee8256df25c6602471092a65aa6
Oleksandr Hrytsenko <oleksandrhry@wizardsdev.com>
2020-09-21 23:56:35 +0300

    add readme

--- src/components/Form/Formik/FormikForm.jsx
+++ src/components/Form/Formik/FormikForm.jsx
@@ -1,76 +1,72 @@
-import React, {useCallback} from 'react';
-import { useFormik } from 'formik';
-import { validationSchema }  from './validationSchema';
-import '../../../App.css';
+import React, { useCallback } from "react";
+import { useFormik } from "formik";
+import { validationSchema } from "./validationSchema";
+import "../../../App.css";
 
-export const  FormikForm = () => {
+export const FormikForm = () => {
+  const formSubmit = () => {
+    alert("Validation succeeded!");
+  };
 
-    const formSubmit = () => {
-        alert("Validation succeeded!")
+  const onKeyPress = useCallback((event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      formSubmit();
     }
+  }, []);
 
-    const onKeyPress = useCallback(
-        event => {
-            if (event.key === 'Enter') {
-                event.preventDefault();
-                formSubmit();
-            }
-        },
-        [],
-    );
+  const formik = useFormik({
+    initialValues: {
+      gmail: "",
+      firstName: "",
+      lastName: "",
+      phone: "",
+    },
+    validationSchema: { validationSchema },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
-    const formik = useFormik({
-        initialValues: {
-            gmail: '',
-            firstName: '',
-            lastName: '',
-            phone: '',
-        },
-        validationSchema: { validationSchema },
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
-
-    return (
-        <form onSubmit={formik.handleSubmit} className="formik-form">
-            <label htmlFor="e-mail">Email Address </label>
-            <input
-                id="gmail"
-                name="gmail"
-                type="gmail"
-                onChange={formik.handleChange}
-                value={formik.values.gmail}
-                required="required"
-            />
-            <label htmlFor="firstName">First name</label>
-            <input
-                id="firstName"
-                name="firstName"
-                type="firstName"
-                onChange={formik.handleChange}
-                value={formik.values.firstName}
-                required="required"
-            />
-            <label htmlFor="lastName">Last name</label>
-            <input
-                id="lastName"
-                name="lastName"
-                type="lastName"
-                onChange={formik.handleChange}
-                value={formik.values.lastName}
-                required="required"
-            />
-            <label htmlFor="phoneNumber">Phone number</label>
-            <input
-                id="phoneNumber"
-                name="phoneNumber"
-                type="phoneNumber"
-                onChange={formik.handleChange}
-                value={formik.values.phone}
-                onKeyPress={onKeyPress}
-            />
-            <button type="submit">Submit</button>
-        </form>
-    );
+  return (
+    <form onSubmit={formik.handleSubmit} className="formik-form">
+      <label htmlFor="e-mail">Email Address </label>
+      <input
+        id="gmail"
+        name="gmail"
+        type="gmail"
+        onChange={formik.handleChange}
+        value={formik.values.gmail}
+        required="required"
+      />
+      <label htmlFor="firstName">First name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="firstName"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+        required="required"
+      />
+      <label htmlFor="lastName">Last name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="lastName"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+        required="required"
+      />
+      <label htmlFor="phoneNumber">Phone number</label>
+      <input
+        id="phoneNumber"
+        name="phoneNumber"
+        type="phoneNumber"
+        onChange={formik.handleChange}
+        value={formik.values.phone}
+        onKeyPress={onKeyPress}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
--- src/components/Form/Formik/validationSchema.js
+++ src/components/Form/Formik/validationSchema.js
@@ -1,19 +1,14 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 export const validationSchema = Yup.object().shape({
-    email: Yup.string()
-        .email('Invalid email')
-        .required('Required'),
-    firstName: Yup.string()
-        .min(2, 'Too Short!')
-        .max(50, 'Too Long!')
-        .required('Required'),
-    lastName: Yup.string()
-        .min(2, 'Too Short!')
-        .max(50, 'Too Long!')
-        .required('Required'),
-    phone: Yup.string()
-        .min(0, 'Too Short!')
-        .max(16, 'Too Long!')
-        // .required('Required'),
+  email: Yup.string().email("Invalid email").required("Required"),
+  firstName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  lastName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  phone: Yup.string().min(0, "Too Short!").max(16, "Too Long!"),
 });
